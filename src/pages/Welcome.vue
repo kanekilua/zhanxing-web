613a4790693fb240aacc3dd1a276b31709211705
Kane <kiluahaibin@gmail.com>
2018-10-29 18:50:46 +0800

    develop Welcome page

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>123</div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+    name : 'welcome'
 }
 </script>
+<style scoped>
 
-<style>
 </style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Welcome from '@/pages/Welcome'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path : '/',
+      name : 'welcome',
+      component : Welcome
+    }
   ]
 })
