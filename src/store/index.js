032343631d4e615f94f383a48daaecdcacf9f549
Kane <kiluahaibin@gmail.com>
2018-11-14 11:11:07 +0800

    optimization the api.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,11 @@
 import Vue from 'vue'
 import vuex from 'vuex'
 Vue.use(vuex);
-
-import login from "./modules/login"
-import register from "./modules/register"
-import resetPwd from "./modules/resetPwd"
-
 export default new vuex.Store ({
     state : {
         loginAccount : ''
     },
     modules : {
-        login : login,
-        register : register,
-        resetPwd : resetPwd
     },
     mutations: {
         updateLoginAccount: function (state,value) {
